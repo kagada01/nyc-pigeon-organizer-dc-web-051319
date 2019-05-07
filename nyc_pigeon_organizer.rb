b85e489ca9886fdcb452a3597c9459e4d4d67307
Dan Kagan <dankagan7@gmail.com>
2019-05-07 18:41:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
       pigeons.each do |pigeon|
         storage[pigeon] ||= {}
         storage[pigeon][key] ||= []
-        #binding.pry
+        binding.pry
       end
     end
   end
