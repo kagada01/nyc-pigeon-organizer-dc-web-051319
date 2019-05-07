bf165fa6220c09bff62a67f3b51b177e36d0155e
Dan Kagan <dankagan7@gmail.com>
2019-05-07 18:31:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, hash), result| 
+    hash.each do |category, pigeons|
+      pigeons.each do |pigeon|
+        result[pigeon] ||= {}
+        binding.pry
+      end
+    end
+  end
 end
