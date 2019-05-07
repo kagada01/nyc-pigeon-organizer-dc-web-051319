b49d14214d79be71b41459a9cee6d18b85e2ebd3
Dan Kagan <dankagan7@gmail.com>
2019-05-07 18:39:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, hash), result| 
+  data.each_with_object({}) do |(key, hash), storage| 
     hash.each do |category, pigeons|
       pigeons.each do |pigeon|
-        result[pigeon] ||= {}
+        storage[pigeon] ||= {}
+        storage[name][key] ||= []
         binding.pry
       end
     end
